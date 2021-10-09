4a7b778d471f6f52ed04ce5d94edd10352e94ef3
xrgs <563805325@qq.com>
2021-10-09 17:11:49 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @create 2021/10/9-17:00
+ */
+
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
