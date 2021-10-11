9abd254ef5c584a1b5c6205dd9be4a80f5a333e2
xrgs <563805325@qq.com>
2021-10-11 20:08:03 +0800

    hot fix commit2

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -9,6 +9,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git4!");
+        System.out.println("hot fix test");
         System.out.println("更新1");
     }
 }
